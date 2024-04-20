ce01e63ba91c58bc0fb706951304a248d99e6742
margin-dev <margindev@outlook.com>
2024-04-20 14:15:21 +0300

    Update

--- popup.js
+++ popup.js
@@ -1,8 +1,8 @@
 window.addEventListener("load", (event) => {
-    $(".PopupSlides.bottom> a:gt(0)").hide();
-    $(".PopupSlides.bottom > a:first").slideDown(500).next().slideUp(500).end().appendTo(".PopupSlides.bottom");
+    $(".PopupSlides> a:gt(0)").hide();
+    $(".PopupSlides > a:first").slideDown(500).next().slideUp(500).end().appendTo(".PopupSlides");
     setInterval(function () {
-        $(".PopupSlides.bottom > a:first").slideDown(500).next().slideUp(500).end().appendTo(".PopupSlides.bottom")
+        $(".PopupSlides > a:first").slideDown(500).next().slideUp(500).end().appendTo(".PopupSlides")
     }, 3000)
 
     const xs = document.querySelectorAll(".popup > .close-popup");
