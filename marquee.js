34b7126b397997612e4dc5a52b12bb04b5c8cf5b
margin-dev <163635112+margin-dev@users.noreply.github.com>
2024-04-21 21:14:48 +0300

    Update marquee.js

--- marquee.js
+++ marquee.js
@@ -1,3 +1,12 @@
-window.addEventListener("load",()=>{
-  function mrq(sc, s) { const psc = document.querySelector(sc); const cl = psc.innerHTML; const fe = psc.children[0]; let i = 0; psc.insertAdjacentHTML("beforeend", cl); psc.insertAdjacentHTML("beforeend", cl); setInterval(function () { fe.style.marginLeft = `-${i}px`; if (i > fe.clientWidth) { i = 0 } i = i + s }, 0) }
-})
+window.mrq = (sc, s) => {
+  const psc = document.querySelector(sc);
+  const cl = psc.innerHTML; 
+  const fe = psc.children[0]; 
+  let i = 0; psc.insertAdjacentHTML("beforeend", cl);
+  psc.insertAdjacentHTML("beforeend", cl);
+  setInterval(function () {
+    fe.style.marginLeft = `-${i}px`;
+    if (i > fe.clientWidth) { i = 0 } i = i + s 
+  }, 0)
+}
+
