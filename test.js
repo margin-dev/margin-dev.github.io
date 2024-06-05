01a0b9e346790a6d0626381a62c07b2fb52ce721
margin-dev <margindev@outlook.com>
2024-06-05 23:12:58 +0300

    Update test.js

--- test.js
+++ test.js
@@ -59,9 +59,9 @@ function createItems(config) {
         return `
         <a href="${item.link}" target="${item.openInNewTab ? '_blank' : '_self'}}" class="popup ${config.position} ${item.brand}} ${index == 0 ? 'active' : ''}">
             ${config.close ?? `<button class="close-popup">X</button> `}
-            ${item.image?.enabled && item.image.enabled != false ? `<img src=${item.image?.link ? item.image.link : `https://margin-dev.github.io/brands/${toTitleCase(item.brand)}.png`}>` : ""}
+            ${!item.image ? `<img src="https://margin-dev.github.io/brands/${toTitleCase(item.brand)}.png">` : item.image}
             ${createOffers(item.offers)}
-            ${item.action?.enabled ?? `<span>${item.action.text}</span>`}
+            <span>${config.action}</span>
         </a>`
     })
 }
