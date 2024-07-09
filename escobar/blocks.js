e9587af5f4929d78f81c5a28b6fece1368ae41bf
Ertuğrulcan Bakas <margindev@outlook.com>
2024-07-09 09:38:41 +0300

    esco

---
+++ escobar/blocks.js
@@ -0,0 +1,51 @@
+import { toTitleCase } from "../utils.js";
+export const config = {
+  items: [
+    {
+      brand: "kralbet",
+      link: "",
+      openInNewTab: true,
+      vip: true,
+      shine: false,
+      image: "",
+    },
+    {
+      brand: "zbahis",
+      link: "",
+      openInNewTab: true,
+      vip: true,
+      shine: false,
+      image: "",
+    },
+    {
+      brand: "fixbet",
+      link: "",
+      openInNewTab: true,
+      vip: true,
+      shine: false,
+      image: "",
+    },
+  ],
+};
+
+function createItems(config) {
+  return config.items
+    .map((item, index) => {
+      item.openInNewTab = item.openInNewTab ? item.openInNewTab : true;
+      return `
+        <a class="block ${item.shine ? "shine" : ""} ${item.brand}" href="${item.link}" target="${item.openInNewTab ? "_blank" : "_self"}">
+            ${!item.image ? `<img src="/escobar/img/${toTitleCase(item.brand)}.png">` : item.image}
+        </a>`;
+    })
+    .join("");
+}
+export function createBlocks(config) {
+  console.log("creating blocks");
+  try {
+    let parent = document.querySelector("section");
+    parent.insertAdjacentHTML("beforeend", createItems(config));
+  } catch (error) {
+    console.log(error);
+  }
+  console.log("blocks created");
+}
