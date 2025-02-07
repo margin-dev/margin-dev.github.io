a175cf7fb2875c3131021836f5a51b8cbb11054f
Margin Developer <margindev@outlook.com>
2025-02-07 17:43:06 +0300

    mrq

---
+++ mrq/mrq-2.js
@@ -0,0 +1,42 @@
+function mrq(sc, s) {
+    const psc = document.querySelector(sc);
+    const cl = psc.innerHTML;
+    const fe = psc.children[0];
+    let i = 0;
+    psc.insertAdjacentHTML("beforeend", cl);
+    psc.insertAdjacentHTML("beforeend", cl);
+    setInterval(function () {
+        fe.style.marginLeft = `-${i}px`;
+        if (i > fe.clientWidth) {
+            i = 0;
+        }
+        i = i + s;
+    }, 0);
+}
+
+function createItems(config) {
+    return config.items
+        .map((item, index) => {
+            return `
+            <a href="${item.link}" class="brand-marquee ${item.brand.toLowerCase()} ${item.class} ${index == 0 ? "active" : ""}" target="_blank" ${config.background ? `style="background:${config.background}"` : ""}>
+                <img class="brand-marquee-brand" src="https://margin-dev.github.io/brands/${item.brand}.png"/>
+                ${item.action && `<div class="brand-marquee-action">${item.action}</div>`}
+            </a>`;
+        })
+        .join("")
+}
+export function createMrq2(config) {
+    try {
+        let mrqs = `
+          <div class="brand-marquee-list">
+              ${createItems(config)}
+          </div>    
+          `;
+        let parent = document.querySelector("script[id='mrqs']").parentNode;
+        parent.insertAdjacentHTML("beforeend", mrqs);
+        parent.classList.add("mrq-parent");
+    } catch (error) {
+        console.log(error);
+    }
+    mrq(`.mrqs > .mrq`, config.speed)
+}
