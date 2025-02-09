09f8d01cd0fe8e7f8e662dcc5caf8921fb98d17d
Margin Developer <margindev@outlook.com>
2025-02-09 22:14:02 +0300

    asd

--- slides/js.js
+++ slides/js.js
@@ -23,11 +23,11 @@ export function initializeSlides(config) {
     const slides = createSlides(config);
     let parent = document.querySelector("script[id='slides']").parentNode
     parent.insertAdjacentHTML("beforeend", slides);
-    const root = document.documentElement;
-    const slidesElementsDisplayed = getComputedStyle(root).getPropertyValue("--slides-elements-displayed");
-    const slidesContent = document.querySelector("ul.slides-content");
-    root.style.setProperty("--slides-elements", slidesContent.children.length);
-    for (let i = 0; i < slidesElementsDisplayed; i++) {
-        slidesContent.appendChild(slidesContent.children[i].cloneNode(true));
-    }
+    // const root = document.documentElement;
+    // const slidesElementsDisplayed = getComputedStyle(root).getPropertyValue("--slides-elements-displayed");
+    // const slidesContent = document.querySelector("ul.slides-content");
+    // root.style.setProperty("--slides-elements", slidesContent.children.length);
+    // for (let i = 0; i < slidesElementsDisplayed; i++) {
+    //     slidesContent.appendChild(slidesContent.children[i].cloneNode(true));
+    // }
 }
