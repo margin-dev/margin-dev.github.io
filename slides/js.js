80b328ec02d50f5773d27baa32c37beb82d82de7
Margin Developer <margindev@outlook.com>
2025-05-20 17:58:59 +0300

    slides

---
+++ slides/js.js
@@ -0,0 +1,29 @@
+import { toTitleCase } from "../utils.js";
+export function createSlides(config) {
+    let items = config.items.map((item) => {
+        return `
+        <a href="${item.link}" target="_blank" class="${item.class} ${item.brand.toLowerCase()}">
+            <img src="https://margin-dev.github.io/brands/${toTitleCase(item.brand)}.png">
+            <span>${item.action}</span>
+        </a>
+        `
+    })
+    return `
+    <div class="slides">
+        ${config.leftItem ? `<div class="slides__left">${config.leftItem}</div>` : ""}
+        <div class="slides__group">
+            ${items.join("")}
+        </div>
+        <div class="slides__group">
+            ${items.join("")}
+        </div>
+        ${config.rightItem ? `<div class="slides__right">${config.rightItem}</div>` : ""}
+    </div>
+    `
+}
+export function initializeSlides(config) {
+    const slides = createSlides(config);
+    let parent = document.querySelector("script[id='slides']").parentNode
+    parent.classList.add("slides-parent")
+    parent.insertAdjacentHTML("beforeend", slides);
+}
