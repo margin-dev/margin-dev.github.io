5d2772dc8831cc6c5675d0e261e10e6fe1ba9375
Margin Developer <margindev@outlook.com>
2025-05-20 17:57:07 +0300

    bundle

---
+++ bundle/js.js
@@ -0,0 +1,13 @@
+import * as AlertCore from "../alert/alert.js";
+import * as MRQCore from "../mrq/mrq.js";
+import * as PopupCore from "../popup/popup.js";
+import * as SliderCore from "../slider/slider.js";
+import * as SlidesCore from "../slides/js.js";
+import * as UtilsCore from "../utils.js";
+
+export const Alert = AlertCore;
+export const MRQ = MRQCore;
+export const Popup = PopupCore;
+export const Slider = SliderCore;
+export const Slides = SlidesCore;
+export const Utils = UtilsCore;
